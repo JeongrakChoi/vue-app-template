f4ec6cdd06a1540fee3b4d9eeeaebd9cc89ee312
Jeongrak Choi <jrchoi0123@gmail.com>
2022-10-06 08:30:14 +0900

    헤더 레이아웃 추가

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="header">
         <h1>Header</h1>
-        <router-link to="/">Home</router-link> / <router-link to="/bar">Go to Bar</router-link>
     </div>
 </template>
 
