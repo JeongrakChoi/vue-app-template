5d63690b070ee476f6bdd6e25c6f349d1ca4dd9d
Jeongrak Choi <jrchoi0123@gmail.com>
2022-10-06 08:30:26 +0900

    홈 메인 구성

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,7 @@
     <div>
         <div>
             <h1>메인 페이지</h1>
-            <p>{msg}</p>
+            <Example/>
         </div>
         <br/>
         <router-link to="/Login">로그인 화면으로</router-link>
@@ -10,15 +10,22 @@
 </template>
 
 <script>
+    import Example from '@/components/Example.vue';
+
     export default {
         name: '메인페이지 입니다',
         props: {
             msg: String
+        },
+        components: {
+            Example
         }
     }
 </script>
 
 <style scoped>
-
+    div{
+        background:#dcdcdc
+    }
 </style>
   
